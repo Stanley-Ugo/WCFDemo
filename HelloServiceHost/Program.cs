062c96436e71d6d24e4e0484642dc63ec635edb6
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-10-30 20:59:18 +0100

    Setting the HelloService as the new ServiceHost object in the Program.cs file

--- HelloServiceHost/Program.cs
+++ HelloServiceHost/Program.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
 
 namespace HelloServiceHost
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-
+            using(ServiceHost host = new ServiceHost(typeof(HelloService.HelloService)))
+            {
+                host.Open();
+                Console.WriteLine("Host started at " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
         }
     }
 }
