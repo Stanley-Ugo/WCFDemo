9a76c9f3acd4e43f0bf2700d2e03fcbd264c1b24
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-11-01 07:30:18 +0100

    Implementing the Save & Get Employee Services

---
+++ EmployeeWCFService/EmployeeWCFService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EmployeeWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EmployeeWCFService" in both code and config file together.
+    public class EmployeeWCFService : IEmployeeWCFService
+    {
+        public Employee GetEmployeeWCF(int Id)
+        {
+            Employee employee = new Employee();
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using(SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spGetEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter parameterId = new SqlParameter();
+                parameterId.ParameterName = "@Id";
+                parameterId.Value = Id;
+                cmd.Parameters.Add(parameterId);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    employee.Id = Convert.ToInt32(reader["Id"]);
+                    employee.Name = reader["Name"].ToString();
+                    employee.Gender = reader["Gender"].ToString();
+                    employee.DateOfBirth = Convert.ToDateTime(reader["DateOfBirth"]);
+                }
+            }
+
+            return employee;
+        }
+
+        public void SaveEmployeeWCF(Employee employee)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using(SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spSaveEmployeeWCF", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter parameterId = new SqlParameter
+                {
+                    ParameterName = "@Id",
+                    Value = employee.Id
+                };
+                cmd.Parameters.Add(parameterId);
+
+                SqlParameter parameterName = new SqlParameter
+                {
+                    ParameterName = "@Name",
+                    Value = employee.Name
+                };
+                cmd.Parameters.Add(parameterName);
+
+                SqlParameter parameterGender = new SqlParameter
+                {
+                    ParameterName = "@Gender",
+                    Value = employee.Gender
+                };
+                cmd.Parameters.Add(parameterGender);
+
+                SqlParameter parameterDateOfBirth = new SqlParameter
+                {
+                    ParameterName = "DateOfBirth",
+                    Value = employee.DateOfBirth
+                };
+                cmd.Parameters.Add(parameterDateOfBirth);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ EmployeeWCFService/IEmployeeWCFService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace EmployeeWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEmployeeWCFService" in both code and config file together.
+    [ServiceContract]
+    public interface IEmployeeWCFService
+    {
+        [OperationContract]
+        Employee GetEmployeeWCF(int Id);
+
+        [OperationContract]
+        void SaveEmployeeWCF(Employee employee);
+    }
+
+}
