42428691dadce31a334b119071e85d6f279ab188
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-11-01 13:33:30 +0100

    Using the DataMember & DataContract Attribute

--- EmployeeWCFService/Employee.cs
+++ EmployeeWCFService/Employee.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EmployeeWCFService
 {
+    [DataContract(Namespace = "http://ugo-omega.com/2020/11/02/Employee")]
     public class Employee
     {
         private int _id;
@@ -13,23 +15,28 @@ namespace EmployeeWCFService
         private string _gender;
         private DateTime _dateOfBirth;
 
+        [DataMember]
         public int Id
         {
             get { return _id; }
             set { _id = value; }
         }
 
+        [DataMember]
         public string Name
         {
             get { return _name; }
             set { _name = value; }
         }
-        
+
+        [DataMember]
         public string Gender
         {
             get { return _gender; }
             set { _gender = value; }
         }
+
+        [DataMember]
         public DateTime DateOfBirth
         {
             get { return _dateOfBirth; }
