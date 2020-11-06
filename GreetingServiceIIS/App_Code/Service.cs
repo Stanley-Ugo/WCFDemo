fb4e9dc3e365219d08fd2f39c82bf203685a4cdd
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-11-06 22:35:30 +0100

    Implementing the Operation

--- GreetingServiceIIS/App_Code/Service.cs
+++ GreetingServiceIIS/App_Code/Service.cs
@@ -9,9 +9,9 @@ using System.Text;
 // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
 public class GreetingServiceIIS: IGreetingServiceIIS
 {
-	public string GetData(int value)
+	public string GetGreeting(string name)
 	{
-		return string.Format("You entered: {0}", value);
+		return "Greetings " + name;
 	}
 
 	public CompositeType GetDataUsingDataContract(CompositeType composite)
